f12f94399004e7822ed496f9bfec12fd1cbc050c
Aare Palm <aarepalm@gmail.com>
2019-05-09 20:38:21 +0300

    Add files via upload

--- PrimesAndFibonacci.java
+++ PrimesAndFibonacci.java
@@ -1,6 +1,12 @@
 class PrimesAndFibonacci {
 
-	boolean isPrime(long integer) {
+	private long n1 = 0, n2 = 1, n3 = 0;
+	private final static long limit = 500000;
+	private final long LUDICROUSLY_BIG_NUMER = 500000000;
+	private final boolean RESET_STATE = false;
+	private final boolean KEEP_STATE = false;
+
+	private boolean isPrime(long integer) {
 		for (long i = 2; i < integer; ++i) {
 			if ((integer % i) == 0) {
 				return false;
@@ -9,29 +15,44 @@ class PrimesAndFibonacci {
 		return true;
 	}
 
-	long doWork(long limit) {
-		final long LUDICROUSLY_BIG_NUMER = 500000000;
-		long n1 = 0, n2 = 1, n3 = 0;
+	private long getNextFibonacci(boolean reset) {
+		if (reset == true) {
+			n1 = 0;
+			n2 = 1;
+			n3 = 0;
+		}
+		n3 = n1 + n2;
+		System.out.print(" " + n3);
+		n1 = n2;
+		n2 = n3;
+		return n3;
+	}
+
+	private boolean foundTheOne(long number) {
+		if ((number > limit) && (isPrime(number)))
+			return true;
+		else
+			return false;
+	}
+
+	private long doWork() {
 		long result = 0;
-		while (n3 < LUDICROUSLY_BIG_NUMER) // So we wouldn't be hanging here, prime check takes long time
+		long fiboNumber = getNextFibonacci(RESET_STATE);
+		while (fiboNumber < LUDICROUSLY_BIG_NUMER) // So we wouldn't be hanging here, prime check takes long time
 		{
-			n3 = n1 + n2;
-			System.out.print(" " + n3);
-			n1 = n2;
-			n2 = n3;
-			if ((n3 > limit) && (isPrime(n3))) {
-				result = n3;
+			if (foundTheOne(fiboNumber)) {
+				result = fiboNumber;
 				break;
 			}
+			fiboNumber = getNextFibonacci(KEEP_STATE);
 		}
 		return result;
 	}
 
 	public static void main(String[] args) {
 		PrimesAndFibonacci primes = new PrimesAndFibonacci();
-		final long limit = 500000;
 		System.out.println("This program calculates first fibonacci number that is bigger than " + limit + " and also a prime");
-		long result = primes.doWork(limit);
+		long result = primes.doWork();
 		System.out.println("\nThe answer is " + ((result != 0) ? result : " N.A"));
 	}
 }
