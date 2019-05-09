023702e2d70de85b407de9a9d2d97475dbbbef05
Aare Palm <aarepalm@gmail.com>
2019-05-09 10:08:38 +0300

    Add files via upload

--- PrimesAndFibonacci.java
+++ PrimesAndFibonacci.java
@@ -1,7 +1,7 @@
 class PrimesAndFibonacci {
 
 	boolean isPrime(long integer) {
-		for (int i = 2; i < integer; ++i) {
+		for (long i = 2; i < integer; ++i) {
 			if ((integer % i) == 0) {
 				return false;
 			}
@@ -9,28 +9,29 @@ class PrimesAndFibonacci {
 		return true;
 	}
 
-	long getNextPrime(long integer) {
-		if (integer < 3)
-			return ++integer;
-
-		while (isPrime(++integer) != true) {
-			// do nothing here
+	long doWork(long limit) {
+		final long LUDICROUSLY_BIG_NUMER = 500000000;
+		long n1 = 0, n2 = 1, n3 = 0;
+		long result = 0;
+		while (n3 < LUDICROUSLY_BIG_NUMER) // So we wouldn't be hanging here, prime check takes long time
+		{
+			n3 = n1 + n2;
+			System.out.print(" " + n3);
+			n1 = n2;
+			n2 = n3;
+			if ((n3 > limit) && (isPrime(n3))) {
+				result = n3;
+				break;
+			}
 		}
-		return integer;
-	}
-
-	long calculatePrime(long limit) {
-		long x = getNextPrime(limit);
-		while (x < limit) {
-			x = getNextPrime(x);
-		}
-		return x;
+		return result;
 	}
 
 	public static void main(String[] args) {
 		PrimesAndFibonacci primes = new PrimesAndFibonacci();
 		final long limit = 500000;
-		System.out.println("This program calculates first prime and fibonacci bigger than " + limit);
-		System.out.println("The prime is " + primes.calculatePrime(limit));
+		System.out.println("This program calculates first fibonacci number that is bigger than " + limit + " and also a prime");
+		long result = primes.doWork(limit);
+		System.out.println("\nThe answer is " + ((result != 0) ? result : " N.A"));
 	}
 }
